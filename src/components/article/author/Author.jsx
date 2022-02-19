803f3a1ee4693ac9fbf45a897dba513033e84bde
Iakovlev Dmitrii <iakovlev.dmitrii@gmail.com>
2022-02-19 22:10:09 +0300

    lint Author

--- src/components/article/author/Author.jsx
+++ src/components/article/author/Author.jsx
@@ -1,75 +1,84 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import PersonDetails from "../../person-details";
 
 import attention from "./images/attention.svg";
-import styles from './Author.module.scss';
+import styles from "./Author.module.scss";
 
-const Author = ({ name, date, avatarSrc, editable, onDeleteClick, articleUrl, isPopUpOpen, onNoClick, onYesClick }) => {
+function Author({
+  name,
+  date,
+  avatarSrc,
+  editable,
+  onDeleteClick,
+  articleUrl,
+  isPopUpOpen,
+  onNoClick,
+  onYesClick,
+}) {
+  const popUp = (
+    <div className={styles.popUp}>
+      <div className={styles.popUpText}>
+        <img src={attention} alt="attention" />
+        <span>Are you sure to delete this article?</span>
+      </div>
 
-    const popUp = (
-        <div className={styles.popUp}>
+      <div className={styles.popUpButtons}>
+        <button className={styles.popUpNo} onClick={onNoClick} type="button">
+          No
+        </button>
+        <button className={styles.popUpYes} onClick={onYesClick} type="button">
+          Yes
+        </button>
+      </div>
+    </div>
+  );
 
-            <div className={styles.popUpText}>
-                <img src={attention} alt='attention' />
-                <span>Are you sure to delete this article?</span>
-            </div>
+  const buttons = (
+    <div className={styles.buttons}>
+      <button
+        className={styles.deleteArticleButton}
+        onClick={onDeleteClick}
+        type="button"
+      >
+        Delete
+      </button>
 
-            <div className={styles.popUpButtons}>
-                <button
-                    className={styles.popUpNo}
-                    onClick={onNoClick}
-                    type='button'>No</button>
-                <button
-                    className={styles.popUpYes}
-                    onClick={onYesClick}
-                    type='button'>Yes</button>
-            </div>
-        </div>
-    );
+      {isPopUpOpen && popUp}
 
-    const buttons = (
-        <div className={styles.buttons}>
-            <button
-                className={styles.deleteArticleButton}
-                onClick={onDeleteClick}
-                type='button'>Delete</button>
+      <Link to={`${articleUrl}/edit`}>
+        <button className={styles.editArticleButton} type="button">
+          Edit
+        </button>
+      </Link>
+    </div>
+  );
 
-            {isPopUpOpen && popUp}
+  return (
+    <div className={styles.author}>
+      <PersonDetails
+        name={name}
+        date={date}
+        src={avatarSrc}
+        alt="user's avatar"
+      />
 
-            <Link to={`${articleUrl}/edit`}>
-                <button
-                    className={styles.editArticleButton}
-                    type='button'>Edit</button>
-            </Link>
-        </div>
-    );
-
-    return (
-        <div className={styles.author}>
-            <PersonDetails
-                name={name}
-                date={date}
-                src={avatarSrc}
-                alt="user's avatar"
-            />
-
-            {editable && buttons}
-        </div>
-    )
-};
+      {editable && buttons}
+    </div>
+  );
+}
 
 Author.propTypes = {
-    name: PropTypes.string.isRequired,
-    date: PropTypes.string.isRequired,
-    avatarSrc: PropTypes.string.isRequired,
-    editable: PropTypes.bool.isRequired,
-    onDeleteClick: PropTypes.func.isRequired,
-    articleUrl: PropTypes.string.isRequired,
-    isPopUpOpen: PropTypes.bool.isRequired,
-    onNoClick: PropTypes.func.isRequired,
-    onYesClick: PropTypes.func.isRequired
+  name: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  avatarSrc: PropTypes.string.isRequired,
+  editable: PropTypes.bool.isRequired,
+  onDeleteClick: PropTypes.func.isRequired,
+  articleUrl: PropTypes.string.isRequired,
+  isPopUpOpen: PropTypes.bool.isRequired,
+  onNoClick: PropTypes.func.isRequired,
+  onYesClick: PropTypes.func.isRequired,
 };
 
 export default Author;
