f72424a34dde70757778441fea7f2f4508b91be7
Iakovlev Dmitrii <iakovlev.dmitrii@gmail.com>
2022-02-23 14:08:00 +0300

    Header with Spinner

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,112 +1,97 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Link, NavLink } from "react-router-dom";
 
-import actionCreators from '../../store/action-creators';
+import Spinner from "../spinner";
 
-import src from './image/User.png';
-import styles from './Header.module.scss';
+import actionCreators from "../../store/action-creators";
 
-const {
-   authButton,
-   authButtonActive,
-   personInfo,
-   personName,
-   personImage
-} = styles;
+import src from "./image/User.png";
+import styles from "./Header.module.scss";
 
-const Header = ({ authentication, logOut }) => {
-
-   const getLinksToShow = () => {
-      if(authentication.isLoggedIn) {
-         const { user } = authentication;
-
-         return (
-             <>
-               <Link
-                  className={styles.createArticleButton}
-                  to='/new-article'>
-                  Create article
-               </Link>
-               <Link to='/profile' className={styles.person}>
-                  <div className={personInfo}>
-                     <div className={personName}>
-                        {user.username}
-                     </div>
-                  </div>
-                  <div className={personImage}>
-                     <img src={user.image || src} alt="user's avatar" />
-                  </div>
-               </Link>
-               <button
-                  type='button'
-                  className={authButton}
-                  onClick={logOut}>
-                  Log Out
-               </button>
-            </>
-         )
-      }
-
+function Header({ user, isLoggedIn, logOut, isLoading }) {
+  const getLinksToShow = () => {
+    if (isLoggedIn) {
       return (
-         <>
-            <NavLink
-               to="/sign-in"
-               className={authButton}
-               activeClassName={authButtonActive}>
-               Sign In
-            </NavLink>
-            <NavLink
-               to="/sign-up"
-               className={authButton}
-               activeClassName={authButtonActive}>
-               Sign Up
-            </NavLink>
-         </>
-      )
-   };
-
-   return (
-      <header className={styles.header}>
-         <div className={styles.container}>
-            <div className={styles.content}>
-               <Link
-                  to='/articles'
-                  className={styles.logo}>Realworld blog</Link>
-               {getLinksToShow()}
+        <>
+          <Link className={styles.createArticleButton} to="/new-article">
+            Create article
+          </Link>
+          <Link to="/profile" className={styles.person}>
+            <div className={styles.personInfo}>
+              <div className={styles.personName}>{user.username}</div>
             </div>
-         </div>
-      </header>
-   )
-};
+            <div className={styles.personImage}>
+              <img src={user.image || src} alt="user's avatar" />
+            </div>
+          </Link>
+          <button type="button" className={styles.authButton} onClick={logOut}>
+            Log Out
+          </button>
+        </>
+      );
+    }
+
+    return (
+      <>
+        <NavLink
+          to="/sign-in"
+          className={styles.authButton}
+          activeClassName={styles.authButtonActive}
+        >
+          Sign In
+        </NavLink>
+        <NavLink
+          to="/sign-up"
+          className={styles.authButton}
+          activeClassName={styles.authButtonActive}
+        >
+          Sign Up
+        </NavLink>
+      </>
+    );
+  };
+
+  return (
+    <div className={styles.header}>
+      <div className={styles.container}>
+        <header className={styles.content}>
+          <Link to="/articles" className={styles.logo}>
+            RealWorld blog
+          </Link>
+          {getLinksToShow()}
+        </header>
+      </div>
+      <div className={styles.spinnerContainer}>{isLoading && <Spinner />}</div>
+    </div>
+  );
+}
 
 Header.propTypes = {
-   authentication: PropTypes.shape({
-      user: PropTypes.shape({
-         username: PropTypes.string,
-         image: PropTypes.string,
-      }),
-      isLoggedIn: PropTypes.bool.isRequired,
-   }),
-   logOut: PropTypes.func.isRequired,
+  user: PropTypes.shape({
+    username: PropTypes.string,
+    image: PropTypes.string,
+  }),
+  isLoggedIn: PropTypes.bool.isRequired,
+  logOut: PropTypes.func.isRequired,
+  isLoading: PropTypes.bool.isRequired,
 };
 
 Header.defaultProps = {
-   authentication: {
-      user: {
-         username: '',
-         image: '',
-      }
-   }
+  user: {
+    username: "",
+    image: "",
+  },
 };
 
-const mapStateToProps = ({authentication}) => ({authentication});
+const mapStateToProps = ({ authentication, loading }) => ({
+  user: authentication.user,
+  isLoggedIn: authentication.isLoggedIn,
+  isLoading: loading.isLoading,
+});
 const mapDispatchToProps = {
-   logOut: actionCreators.authentication.logOut,
+  logOut: actionCreators.authentication.logOut,
 };
 
-export default connect(
-   mapStateToProps,
-   mapDispatchToProps
-)(Header);
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
